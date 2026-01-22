aca06b5c07f13ab638f1242f46635ba67e02f5eb
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2026-01-22 17:23:14 +0300

    add: multi languge support

--- src/Resources/lang/tr/app.php
+++ src/Resources/lang/tr/app.php
@@ -9,10 +9,18 @@ return [
         'description'       => 'Param',
 
         'system' => [
-            'title'             => 'Başlık',
-            'description'       => 'Açıklama',
-            'image'             => 'Logo',
-            'status'            => 'Durum',
+            'title'                => 'Başlık',
+            'description'          => 'Açıklama',
+            'image'                => 'Logo',
+            'status'               => 'Durum',
+            'client_code'          => 'Müşteri Kodu',
+            'client_code_info'     => 'ParamPOS panosundan aldığınız Üye İşyeri Müşteri Kodu girin',
+            'client_username'      => 'Müşteri Kullanıcı Adı',
+            'client_username_info' => 'ParamPOS panosundan aldığınız Üye İşyeri Müşteri Kullanıcı Adı girin',
+            'client_password'      => 'Müşteri Şifresi',
+            'client_password_info' => 'ParamPOS panosundan aldığınız Üye İşyeri Müşteri Şifresi girin',
+            'guid'                 => 'GUID',
+            'guid_info'            => 'ParamPOS panosundan aldığınız Üye İşyeri GUID girin',
         ],
     ],
 
